dfff3294c1dd6cd25aefe373f545c58f512698ce
LUKBAN <lukban4c@gmail.com>
2019-07-17 18:10:22 -0700

    added ingredients and did basic styling

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ class App extends Component {
     _query: "banana"
   };
 
+  /**Update _search from user input */
   handleChange = e => {
     const { name, value } = e.target;
     this.setState({ [name]: value });
@@ -21,21 +22,9 @@ class App extends Component {
   /** Run when form is submitted */
   handleSearch = e => {
     e.preventDefault(); //stop page refresh
-    // console.log(this.state._search);
-    // console.log(this.state._query);
-    // console.log("pause");
-    const { _search, _query } = this.state;
-    /**----ISSUE HERE----- */
-    this.setState({ _query: _search });
-    /**----ISSUE HERE----- */
-    console.log(`_search ${this.state._search}`);
-    console.log(`_query ${this.state._query}`);
-  };
-
-  componentDidMount() {
     fetch(
       `https://api.edamam.com/search?q=${
-      this.state._search
+        this.state._search
       }&app_id=${APP_ID}&app_key=${APP_KEY}`
     )
       .then(response => response.json())
@@ -43,38 +32,14 @@ class App extends Component {
         const { hits } = response;
         this.setState({ _recipe: hits });
         console.log(hits);
-      });
-  }
-
-  // useEffect = () => {
-  //   // Update the document title using the browser API
-  //   this.getRe
-  // };
-
-  // getRecipes = () => {
-  //   fetch(
-  //     `https://api.edamam.com/search?q=${
-  //       this.state._query
-  //     }&app_id=${APP_ID}&app_key=${APP_KEY}`
-  //   )
-  //     .then(response => response.json())
-  //     .then(response => {
-  //       const { hits } = response;
-  //       this.setState({ _recipe: hits });
-  //       console.log(hits);
-  //     });
-  // };
-
-  // com
-
-  // useEffect = () => {
-  //   this.getRecipes(), [this.state._query];
-  // };
+      })
+      .then(this.setState({ _search: "" })); //remove previous search
+  };
 
   render() {
     return (
       <div className="App">
-        <h1>this.state._query value: {this.state._query}</h1>
+        {/** <h1>this.state._query value: {this.state._query}</h1> */}
         <form onSubmit={this.handleSearch} className="search-form">
           <input
             className="search-bar"
@@ -87,14 +52,17 @@ class App extends Component {
             Search
           </button>
         </form>
-        {this.state._recipe.map(food => (
-          <Recipe
-            key={food.recipe.label}
-            title={food.recipe.label}
-            calories={food.recipe.calories}
-            image={food.recipe.image}
-          />
-        ))}
+        <div className="recipes">
+          {this.state._recipe.map(food => (
+            <Recipe
+              key={food.recipe.label}
+              title={food.recipe.label}
+              calories={food.recipe.calories}
+              image={food.recipe.image}
+              ingredients={food.recipe.ingredients}
+            />
+          ))}
+        </div>
       </div>
     );
   }
--- src/Recipe.js
+++ src/Recipe.js
@@ -1,11 +1,17 @@
 import React from "react";
+import style from "./recipe.module.css";
 
 //const Recipe = ({ title, calories, image }) => {
 const Recipe = props => {
   return (
-    <div>
+    <div className={style.recipe}>
       <h1>{props.title}</h1>
-      <p>{props.calories}</p>
+      <o1>
+        {props.ingredients.map(ingredient => (
+          <li>{ingredient.text}</li>
+        ))}
+      </o1>
+      <p>{`Cal: ${Math.floor(props.calories)}`}</p>
       <img src={props.image} alt="" />
     </div>
   );
